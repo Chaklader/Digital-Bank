dc3fea9ce769304aa2ccb0cb5d46890198a697c1
Arefe <omi.chaklader@gmail.com>
2024-04-21 15:18:31 +0600

    feat: add account and transfer CRUD operations, and CI pipeline
    - Implement CRUD queries for accounts and transfers using SQLC
    - Set up GitHub Actions for running unit tests with PostgreSQL

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+## Database
+
+Enter inside the DB `simple_bank` in the docker container:
+
+```shell
+docker exec -it postgres psql -U root -d simple_bank
+```
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@127.0.0.1:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := pgx.Connect(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("Unable to connect to the database:", err)
+	}
+	defer func(conn *pgx.Conn, ctx context.Context) {
+		err := conn.Close(ctx)
+		if err != nil {
+
+		}
+	}(conn, context.Background())
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
