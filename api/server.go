2e3014ebda00759161eda12e0498fef5a0ec863a
Arefe <omi.chaklader@gmail.com>
2024-04-24 11:58:12 +0600

    GB: create, get and list accounts functionalities are implemented

--- api/server.go
+++ api/server.go
@@ -51,13 +51,13 @@ func (server *Server) setupRouter() {
 
 	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
-	//router.POST("/tokens/renew_access", server.renewAccessToken)
+	router.POST("/tokens/renew_access", server.renewAccessToken)
 
-	//authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
-	//authRoutes.POST("/accounts", server.createAccount)
-	//authRoutes.GET("/accounts/:id", server.getAccount)
-	//authRoutes.GET("/accounts", server.listAccounts)
-	//
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccounts)
 	//authRoutes.POST("/transfers", server.createTransfer)
 
 	server.router = router
